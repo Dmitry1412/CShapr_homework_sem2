23d7fc02f31e4e6aa2601f423648328bbd735df1
Dmitry <sicily007@rambler.ru>
2023-08-05 21:49:39 +0300

    Create_example_010

---
+++ example_010/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+
+Вариант решения через индекс элемента строки
+string num = Console.ReadLine();
+if (num < 100 | num > 999)
+{
+    System.Console.WriteLine("Введите число от 100 до 999");
+}
+else
+{
+    System.Console.WriteLine(num[1]);
+}
+*/
+
+int num = int.Parse(Console.ReadLine());
+if (num < 100 | num > 999)
+{
+    System.Console.WriteLine("Введите число от 100 до 999");
+}
+else
+{
+    System.Console.WriteLine(num % 100 / 10);
+}
+
+
+
+
